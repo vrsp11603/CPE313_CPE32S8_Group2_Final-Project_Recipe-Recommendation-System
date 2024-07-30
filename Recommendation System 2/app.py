6ba7a24194a8439413d924c1b303d4927ef92a87
qvrspena <144860191+qvrspena@users.noreply.github.com>
2024-07-30 22:03:22 +0800

    Update app.py

--- Recommendation System 2/app.py
+++ Recommendation System 2/app.py
@@ -5,23 +5,23 @@
 # import streamlit.components.v1 as stc
 
 # YAML library is used for reading classes of inventory
-import yaml
+# import yaml
 
-# For data manipulation
-import pandas as pd
-import numpy as np
+# # For data manipulation
+# import pandas as pd
+# import numpy as np
 
-# Used for applying YOLO object detection models
-from ultralytics import YOLO
+# # Used for applying YOLO object detection models
+# from ultralytics import YOLO
 
-# Used for viewing uploaded files on streamlit
-import PIL
+# # Used for viewing uploaded files on streamlit
+# import PIL
 
-# RE library is for string matcher that is used for finding quantities
-import re
+# # RE library is for string matcher that is used for finding quantities
+# import re
 
-# Unicodedata is used for converting unicode fraction data
-import unicodedata
+# # Unicodedata is used for converting unicode fraction data
+# import unicodedata
 
 def main():
     st.title("Recipe Recommendation System")
