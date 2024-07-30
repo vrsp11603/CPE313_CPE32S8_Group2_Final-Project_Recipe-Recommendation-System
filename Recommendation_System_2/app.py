691bb98623b226652f6e21d34408207babb9b29b
qvrspena <144860191+qvrspena@users.noreply.github.com>
2024-07-30 22:08:15 +0800

    Create app.py

---
+++ Recommendation_System_2/app.py
@@ -0,0 +1,30 @@
+# Libraries
+
+# Streamlit is used for creating interactive web apps
+import streamlit as st
+import streamlit.components.v1 as stc
+
+# YAML library is used for reading classes of inventory
+import yaml
+
+# # For data manipulation
+import pandas as pd
+import numpy as np
+
+# # Used for applying YOLO object detection models
+from ultralytics import YOLO
+
+# # Used for viewing uploaded files on streamlit
+import PIL
+
+# # RE library is for string matcher that is used for finding quantities
+import re
+
+# # Unicodedata is used for converting unicode fraction data
+import unicodedata
+
+def main():
+    st.title("Recipe Recommendation System")
+
+if __name__ == '__main__':    
+    main()
